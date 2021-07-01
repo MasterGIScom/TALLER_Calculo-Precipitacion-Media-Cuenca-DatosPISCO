92223a38414409567ce8cd94959341d75765c666
MasterGIS <80126056+MasterGis01@users.noreply.github.com>
2021-07-01 10:59:30 -0500

    Update PrecMean.R
    pm

--- PrecMean.R
+++ PrecMean.R
@@ -1,28 +1,30 @@
 library(raster)
 library(sp)
 library(sf)
-library(latticeExtra)
 library(ncdf4)
 library(rgdal)
 library(dplyr)
 setwd("D:/MASTERGIS/PISCO")
 # ..load the data .nc
-raster_pp<-"DATOS PISCO/UnsPrecMonthly.nc" %>% raster::brick()
+raster_Pisco <- '' %>% raster::brick()
 spplot(pisco_pm[[1]])
 
 #...vector type 
-cuanca_Ilave = readOGR(dsn = ".", layer = "santaWGS84")
-plot(cuanca_Ilave, 
+cuanca_santa <-  readOGR()
+plot(cuanca_santa, 
      axes = T, 
      asp=1, 
      col = "Cyan", 
-     main = "Cuenca Ilave")
+     main = "Cuenca del rio Santa")
 #...extract the mean precipitation values of the basin
-pm_cuenca_Ilave = extract(x = pisco_pm,y = cuanca_Ilave, fun = mean)
+pm_cuenca_santa <-  extract(x = raster_Pisco,y = cuanca_santa, fun = mean)
 
-plot(pm_cuenca_Ilave[1,],
-     type = "l", col = "blue", ylim = c(0,300),
-     ylab = "Precipitacion", xlab= "Tiempo", main = "Precipitacion media Areal Cuenca Ilave")+
-  grid()
+plot(pm_cuenca_santa[1,],
+     type = "l", 
+     col = "blue", 
+     ylim = c(0,300),
+     ylab = "Precipitacion media mensual", 
+     xlab= "Tiempo en meses del año 1981 al 2021", 
+     main = "Precipitacion media Areal Cuenca Santa")+ grid()
 #...Export the values in csv file 
-write.csv(t(pm_cuenca_Ilave), "Precipitacion_media_Areal_ilave.csv")
+write.csv(t(pm_cuenca_santa), "EXCEL/Pm_santa.csv")
