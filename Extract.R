502ccb2e064b8b520287e418dcc310377289f7e9
MasterGIS <80126056+MasterGis01@users.noreply.github.com>
2021-07-01 10:58:31 -0500

    Update Extract.R
    code

--- Extract.R
+++ Extract.R
@@ -4,30 +4,33 @@ library(ncdf4)
 library(readxl) #Excel
 library(raster)
 library(rgdal)
-setwd("D:/MASTERGIS/PISCO")
+setwd("")
 #..load the folder or file
-Longitud_Latitud <- "EXCEL/Cooderanda.xlsx" %>% 
+coor <- "EXCEL/coordenadas.xlsx" %>% 
   read_excel() %>% arrange_all()
+
 # ..load the data .nc
-raster_pp<-"DATOS PISCO/UnsPrecMonthly.nc" %>% raster::brick()
+pisco<-"DATOS PISCO/UnsPrecMonthly.nc" %>% raster::brick()
+
 #...coordinate assignement
-sp::coordinates(Longitud_Latitud)<- ~XX+YY
-plot(Longitud_Latitud)
+sp::coordinates(coor)<- ~X+Y
+
 # match the projection of raster with point to extract
-raster::projection(Longitud_Latitud)<- raster::projection(raster_pp)
+raster::projection(coor)<- raster::projection(pisco)
 #...Extract the values
-points_long_lati<- raster_pp[[1]] %>% raster::extract(Longitud_Latitud, cellnumbers=T)[,1]
-data_long_lati<-raster_pp[points_long_lati] %>% t()
-colnames(data_long_lati)<-as.character(Longitud_Latitud$NN) #assigment of subbasin colnames
-View(data_long_lati)
+point_values<- raster::extract(pisco[[1]], coor, cellnumbers=T)[,1]
+data_values<-t(pisco[point_values])
+
+#assigment of subbasin colnames
+colnames(data_values)<-as.character(coor$Nombre) 
+
 #...Export the values in csv file 
-write.csv (data_long_lati, "EXCEL/DailyRainfall2.csv")
+write.csv (data_values, "EXCEL/PrecMonthlySanta.csv")
 #Display for the all the precipitation date
-plot(as.numeric(data_long_lati),
+plot(as.numeric(data_values),
      type="p",
      col="#1B9E77",
      lwd = 3,
      xlab="Número de datos",
      main='Precipitaciones de las cuenca del Rio Santa',
      ylab="Precipitaciones Mensuales (mm)")
-length(as.numeric(data_long_lati))
